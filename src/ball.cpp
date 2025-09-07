d0f07fa40c485fd29376958fb148b711480bba80
Jose Alberto <joseal9706@gmail.com>
2025-09-07 17:47:05 -0600

    Moving to raylib deleting everything we had before

--- src/ball.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Jose Alberto Barrantes on 23/8/25.
-//
-
-#include "../Includes/ball.h"
-
-ball::ball(const int locX, const int locY, const int radius) :
-locX(locX), locY(locY), radiusSize(radius)
-{}
